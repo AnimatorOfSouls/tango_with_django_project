ea41c1425d4e041909e55272bc72e887e8a60ef4
AnimatorOfSouls <fantailfishy@gmail.com>
2021-02-08 11:40:32 +0000

    TEST 10 passed

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
+from datetime import datetime
+
 
 
 def index(request):
@@ -18,12 +20,20 @@ def index(request):
     context_dict["categories"] = category_list
     context_dict["pages"] = page_list
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by AnimatorOfSouls.'}
+    context_dict = {}
+    context_dict["boldmessage"] = "This tutorial has been put together by AnimatorOfSouls."
+
+    visitor_cookie_handler(request)
+    context_dict["visits"] = request.session['visits']
+
     return render(request, 'rango/about.html', context=context_dict)
 
 
@@ -159,3 +169,30 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
+
+
+
+
+
+### COOKIES
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+
+    return val
+
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
